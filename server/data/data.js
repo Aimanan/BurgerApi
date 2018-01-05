e52f935307d7c1fe3a2ba74ae307ceebe175583b
Petar Bochukov <petar.bochukov@abv.bg>
2018-01-05 22:23:41 +0200

    minor change

--- server/data/data.js
+++ server/data/data.js
@@ -1,11 +1,9 @@
 const BurgerData = require('./burger.data');
-//const PostData = require('./posts.data');
 
-const init = (db) => {
-    return Promise.resolve({
-        burgers: new BurgerData(db),
-        //posts: new PostData(db)
-    });
+const init = async (db) => {
+    return {
+        burgers: new BurgerData(db)
+    };
 };
 
 module.exports = { init };
